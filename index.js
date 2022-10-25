9788846b4dbd3d77248102003e144309f9b9446f
chanchal26 <chanchal.mch2613@gmail.com>
2022-10-26 03:23:37 +0600

    server side almost done

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ const allCourses = require("./Data/Courses.json");
 app.get("/", (req, res) => {
     res.send("Now server is running");
 });
+
+app.get("/courses", (req, res) => {
+    res.send(allCourses);
+});
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const getSingleCourse = allCourses?.find((p) => p.id == id);
+    if (!getSingleCourse) {
+        res.send("Porduct khuje pai nai");
+    }
+    res.send(getSingleCourse);
+});
+
+
+app.listen(Port, () => {
+    console.log("server is running", Port);
+});

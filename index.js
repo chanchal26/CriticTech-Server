e8c0189d21ec51ffb5964bfe6bc5311900b08951
chanchal26 <chanchal.mch2613@gmail.com>
2022-10-26 03:22:27 +0600

    set the port

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+const allCourses = require("./Data/Courses.json");
+
+app.get("/", (req, res) => {
+    res.send("Now server is running");
+});
